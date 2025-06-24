6484a2cc781806f149c242a2e14938d39be5b056
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-06-24 14:06:52 +0530

    disconnect

--- src/app/api/timeline/route.js
+++ src/app/api/timeline/route.js
@@ -5,14 +5,31 @@ export async function POST(req) {
     console.log(project, scene, timeline)
 
     const r3 = new R3SpaceEngine('localhost', 9010)
-    await r3.connect()
+    await r3.connect();
+    r3.setDebug(true);
 
     const sceneObj = await r3.loadScene(project, scene);
 
     if (!sceneObj) {
         return new Response(JSON.stringify({ error: "Scene not loaded" }), { status: 404 })
     }
-    sceneObj.takeOnline()
-    await sceneObj.playTimeline(timeline) // timeline = "In" or "Out"
+
+
+
+
+    if (timeline === "In") {
+        sceneObj.takeOnline()
+        await sceneObj.playTimeline(timeline) // timeline = "In" or "Out"
+    }
+    else {
+        await sceneObj.playTimeline(timeline) // timeline = "In" or "Out"
+        setTimeout(() => {
+            sceneObj.takeOffline()
+            r3.disconnect();
+        }, 2000) // wait for 1 second before taking offline;
+
+
+    }
+
     return new Response(JSON.stringify({ status: `Played timeline ${timeline}` }))
 }
